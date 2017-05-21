9e580a08dc99bcb624807e8797348450c502fdb2
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-05-21 13:54:03 +0700

    Add loading icon

--- web_ui/app/assets/javascripts/images.js
+++ web_ui/app/assets/javascripts/images.js
@@ -12,7 +12,6 @@ socket.on('answer', function(message){
   })
 });
 
-
 // get answer about nutrition of food
 socket.on('nutrition', function(message){
   var canvas = JSON.parse(JSON.parse(message).context).canvas;
@@ -21,5 +20,5 @@ socket.on('nutrition', function(message){
   var food_content = JSON.parse(JSON.parse(message).context).data;
   $('.js--food-content').html(food_content.content);
   $('.js--food-image').attr("src",food_content.img);
-  
+  $('.animation-load').fadeOut(400);
 });
