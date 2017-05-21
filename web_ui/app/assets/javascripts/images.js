406475c8f5bf2e996b65d74edc6646a07dae93fb
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-05-21 07:22:18 +0700

    UI show map

--- web_ui/app/assets/javascripts/images.js
+++ web_ui/app/assets/javascripts/images.js
@@ -3,7 +3,6 @@
 // var socket = io.connect('{{SOCKET_SERVER_URI}}:1333');
 var socket = io.connect('0.0.0.0:1333');
 socket.on('answer', function(message){
-  console.log(message);
   $.ajax({
     method: "POST",
     url: "/images/return_data",
